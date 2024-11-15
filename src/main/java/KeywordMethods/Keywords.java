cbd411f68a6b937cc68653a815b0e2232e2cf45d
Vinayreddy129 <vinayreddy6126@gmail.com>
2024-11-15 17:06:24 +0530

    first commit

---
+++ src/main/java/KeywordMethods/Keywords.java
@@ -0,0 +1,46 @@
+package KeywordMethods;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Keywords {
+    WebDriver driver;
+
+    public void openBrowser(String url) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver=new ChromeDriver();
+        driver.get(url);
+        Thread.sleep(2000);
+    }
+
+    public void enterText(String locatorType, String locatorValue, String data) throws InterruptedException {
+        WebElement element = locateElement(locatorType, locatorValue);
+        element.sendKeys(data);
+        Thread.sleep(2000);
+    }
+
+    public void click(String locatorType, String locatorValue) throws InterruptedException {
+        WebElement element = locateElement(locatorType, locatorValue);
+        element.click();
+        Thread.sleep(6000);
+    }
+
+    public WebElement locateElement(String locatorType, String locatorValue) {
+        switch (locatorType.toLowerCase()) {
+            case "id":
+                return driver.findElement(By.id(locatorValue));
+            case "name":
+                return driver.findElement(By.name(locatorValue));
+            case "xpath":
+                return driver.findElement(By.xpath(locatorValue));
+            case "css":
+                return driver.findElement(By.cssSelector(locatorValue));
+            default:
+                throw new IllegalArgumentException("Invalid locator type: " + locatorType);
+        }
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ src/main/java/utilities/ExcelUtils.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import org.apache.poi.ss.usermodel.*;
+import java.io.FileInputStream;
+
+public class ExcelUtils {
+    public static String[][] readExcelData(String filePath, String sheetName) throws Exception {
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = WorkbookFactory.create(fis);
+        Sheet sheet = workbook.getSheet(sheetName);
+        int rows = sheet.getPhysicalNumberOfRows();
+        int cols = sheet.getRow(0).getPhysicalNumberOfCells();
+
+        String[][] data = new String[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < cols; j++) {
+                data[i][j] = row.getCell(j).toString();
+            }
+        }
+        workbook.close();
+        fis.close();
+        return data;
+    }
+}
